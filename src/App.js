71d94078c1ad66de154a5a7acfcbe5b254f8b997
colinclerk <colin@clerk.dev>
2021-05-13 03:01:24 -0400

    Add test case

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useCallback,
+  useLayoutEffect,
+  useEffect,
+  useRef,
+  useMemo,
+} from "react";
+
+function useListener(label, add, remove) {
+  const [time, setTime] = useState(0);
+
+  const listener = useCallback(() => {
+    const newTime = Date.now();
+    console.log(label, "listeners called", newTime);
+    setTime(newTime);
+  }, [setTime]);
+
+  // If you uncomment this hack and comment the add(listener) in
+  // useEffect, then the listener gets added twice, even though
+  // the logging of the listener being added only occurs once.
+
+  // As far as I can tell, the second addition must be happening
+  // during a virtual DOM step.
+
+  // You can see the second addition by manually calling
+  // window.Clerk.outerListeners in console
+
+  // const hack = useState(() => {
+  //   add(listener);
+  //   return 0;
+  // });
+
+  useEffect(() => {
+    add(listener);
+    return () => {
+      remove(listener);
+    };
+  }, [remove, listener, add]);
+  return time;
+}
+
+function Outer({ children }) {
+  const time = useListener(
+    "Outer",
+    window.Clerk.addOuterListener,
+    window.Clerk.removeOuterListener
+  );
+  console.log("Outer render", time);
+  return (
+    <div style={{ backgroundColor: "red", padding: "1rem" }}>
+      <h3>Outer</h3>
+      {time === 0 && children}
+    </div>
+  );
+}
+
+function Inner({ children }) {
+  const time = useListener(
+    "Inner",
+    window.Clerk.addInnerListener,
+    window.Clerk.removeInnerListener
+  );
+  console.log("Inner render", time);
+  return (
+    <div style={{ backgroundColor: "blue", padding: "1rem" }}>
+      <h3>Inner</h3>
+      {children}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Outer>
+      <Inner />
+    </Outer>
   );
 }
 
